9aaa57b7dd1e6fee2ada8c12dda0fb0ed76e8bc7
Alexandre <alexandre@meslin.com.br>
2024-09-16 11:19:22 -0300

    Aula 2024-09-03 insere contato

--- contatos/urls.py
+++ contatos/urls.py
@@ -6,4 +6,5 @@ app_name = 'contatos'
 urlpatterns = [
     path('', views.ContatoListView.as_view(), name='home-contatos'),
     path('lista/', views.ContatoListView.as_view(), name='lista-contatos'),
+    path('cria/', views.ContatoCreateView.as_view(), name='cria-contato'),
 ]
